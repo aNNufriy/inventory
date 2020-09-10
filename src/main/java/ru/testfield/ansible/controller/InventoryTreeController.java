b8cdd7c6b5fd60e066c2d8bfde6e4bd25b26f3ee
Alexander Schedrov <schedrow@gmail.com>
2020-09-10 13:28:39 +0300

    jstree api support added

--- src/main/java/ru/testfield/ansible/controller/InventoryTreeController.java
+++ src/main/java/ru/testfield/ansible/controller/InventoryTreeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class InventoryTreeController {
 
-    @RequestMapping("/tree")
+    @RequestMapping("/")
     public String index(){
         return "ansible/pages/hostTree";
     }
--- src/main/java/ru/testfield/web/controller/InventoryController.java
+++ src/main/java/ru/testfield/web/controller/InventoryController.java
@@ -3,22 +3,27 @@ package ru.testfield.web.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.testfield.ansible.model.AnsibleHost;
 import ru.testfield.ansible.model.AnsibleHostGroup;
 import ru.testfield.ansible.model.JstreeNode;
 import ru.testfield.ansible.repository.AnsibleHostGroupRepository;
+import ru.testfield.ansible.repository.AnsibleHostRepository;
 import ru.testfield.ansible.service.InventoryService;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 public class InventoryController extends AbstractWebController {
 
     private final InventoryService inventoryService;
+    private final AnsibleHostRepository ansibleHostRepository;
     private final AnsibleHostGroupRepository ansibleHostGroupRepository;
 
-    public InventoryController(InventoryService inventoryService, AnsibleHostGroupRepository ansibleHostGroupRepository) {
+    public InventoryController(InventoryService inventoryService, AnsibleHostRepository ansibleHostRepository, AnsibleHostGroupRepository ansibleHostGroupRepository) {
         this.inventoryService = inventoryService;
+        this.ansibleHostRepository = ansibleHostRepository;
         this.ansibleHostGroupRepository = ansibleHostGroupRepository;
     }
 
@@ -28,22 +33,32 @@ public class InventoryController extends AbstractWebController {
     }
 
     @RequestMapping("/inventory/jstree")
-    public Set<JstreeNode> getInventoryJstree(@RequestParam Optional<String> id) {
-        Set<AnsibleHostGroup> groups = ansibleHostGroupRepository.findByParentId(null);
-        Set<JstreeNode> nodes = groups.stream().map(this::getJstreeNode).collect(Collectors.toSet());
-        for (JstreeNode node : Set.copyOf(nodes)) {
-            if(node.getId()!=null) {
-                Set<AnsibleHostGroup> ngr = ansibleHostGroupRepository.findByParentId(UUID.fromString(node.getId()));
-                nodes.addAll(ngr.stream().map(this::getJstreeNode).collect(Collectors.toSet()));
-            }
-        }
+    public Set<JstreeNode> getInventoryJstree(@RequestParam String id) {
+        Iterable<AnsibleHostGroup> allGroups = ansibleHostGroupRepository.findAll();
+        Set<JstreeNode> nodes = new TreeSet<>();
+        nodes.addAll(StreamSupport.stream(allGroups.spliterator(), false)
+                .map(this::getJSTreeNode)
+                .collect(Collectors.toSet()));
+        Iterable<AnsibleHost> allHosts = ansibleHostRepository.findAll();
+        nodes.addAll(StreamSupport.stream(allHosts.spliterator(), false)
+                .map(this::getJSTreeNode)
+                .collect(Collectors.toSet()));
         return nodes;
     }
 
-    private JstreeNode getJstreeNode(AnsibleHostGroup host) {
+    private JstreeNode getJSTreeNode(AnsibleHost ansibleHost) {
         return new JstreeNode(
-                host.getId().toString(),
-                host.getParent() != null ? host.getParent().getId().toString() : "#",
-                host.getName());
+                ansibleHost.getId().toString(),
+                ansibleHost.getGroup() != null ? ansibleHost.getGroup().getId().toString() : "#",
+                ansibleHost.getName(),false,
+                "server");
+    }
+
+    private JstreeNode getJSTreeNode(AnsibleHostGroup group) {
+        return new JstreeNode(
+                group.getId().toString(),
+                group.getParent() != null ? group.getParent().getId().toString() : "#",
+                group.getName(),true,
+                "group");
     }
 }
